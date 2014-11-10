c659b346bfaa1607ad4ea6c391340e2aa4f47677
Parry <Parry@lawn-128-61-59-154.lawn.gatech.edu>
2014-11-10 15:50:36 -0500

    dadsa

---
+++ Predictor.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+
+__author__ = 'Parry'
+from gensim.models import LdaModel
+from gensim import corpora
+from Constants import Parameters
+
+
+
+dictionary = corpora.Dictionary.load(Parameters.Dictionary_path)
+corpus = corpora.MmCorpus(Parameters.Corpus_path)
+lda = LdaModel.load(Parameters.Lda_model_path)
+
+
+
+corpus_collection = MongoClient(Parameters.MONGO_CONNECTION_STRING)[Parameters.REVIEWS_DATABASE][Parameters.CORPUS_COLLECTION]
+
+i=0
+corpus_cursor = corpus_collection.find()
+for review in corpus_cursor:
+             # assume there's one document per line, tokens separated by whitespace
+             i=i+1
+             print lda[dictionary.doc2bow(review["words"])]
+             if i ==20:
+                 break;
+
+
