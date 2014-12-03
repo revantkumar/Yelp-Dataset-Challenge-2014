418657881b13e1fa16820f4dc8b64c994df7c1a9
Ashwini Khare <khare.ashwini@gmail.com>
2014-12-03 03:41:08 -0500

    Added rating constraint

--- getBusinessTopic.py
+++ getBusinessTopic.py
@@ -14,21 +14,24 @@ business_collection = MongoClient(Parameters.MONGO_CONNECTION_STRING)[Parameters
 
 topic_rating_collection = MongoClient(Parameters.MONGO_CONNECTION_STRING)[Parameters.REVIEWS_DATABASE][Parameters.TOPIC_RATING_COLLECTION]
 
-#topic_id = 50
-def find_by_topic_id(topic_id):
+topic_id = 50
+def find_by_topic_id(topic_id, rating):
+    
 #result = topic_rating_collection.find_one()
-    result = topic_rating_collection.find({'ratings.'+str(topic_id):{'$exists':True}})
+    result = topic_rating_collection.find({'ratings.'+str(topic_id):{'$gt': int(rating) }})
 #returns valid collection
     businesses = []
     for a in result:
         fetched_business = {}
-        fetched_business['rating'] =  a['ratings'][str(topic_id)]
+        fetched_business['rating'] =  int(a['ratings'][str(topic_id)])
         b = business_collection.find_one({ '_id' : a['business'] })
         fetched_business['lat'] = b['lat']
         fetched_business['lon'] = b['lon']
         fetched_business['name'] = b['name']
-        businesses.append(fetched_business)
+        if fetched_business['rating'] > rating:
+            businesses.append(fetched_business)
     return businesses
+#print find_by_topic_id(50, 4)
 #print businesses
 #db.TopicRating.findOne({ "ratings.10" : { $exists: true }  })
 #db.BusinessInfo.findOne({ '_id' : 'EmzaQR5hQlF0WIl24NxAZA'})
@@ -42,7 +45,8 @@ def index():
 def getInfo():
     from bottle import response
     topic_id = request.query.topic_id
-    businesses = find_by_topic_id(topic_id)
+    rating = request.query.rating
+    businesses = find_by_topic_id(topic_id, int(rating)-1)
     json_businesses = json.dumps(businesses)
     #response.content_type = 'application/json'
     return json_businesses
