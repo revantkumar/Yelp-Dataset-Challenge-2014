98299c36dea923158afe4b3a59ae695db58e3410
Gopi Krishnan <gopikrishnan.nambiar@gmail.com>
2014-12-02 23:40:22 -0500

    Python script to parse json

---
+++ load_data.py
@@ -0,0 +1,14 @@
+import json
+from pprint import pprint
+json_data=open('json_data.json')
+
+data = json.load(json_data)
+num = 0 
+for i in data:
+	print "\nPlace" + str(num)+":"
+	print i['lat']
+	print i['lon']
+	print i['name']
+	print i['rating']
+	num = num+1
+json_data.close()
